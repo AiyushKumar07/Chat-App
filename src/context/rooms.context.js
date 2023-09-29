58daabbe724c632d9a7d399ff124b19b7b850563
Aiyush Kumar <aiyushkanojia@gmail.com>
2023-09-29 19:27:22 +0530

    Created RoomContext

---
+++ src/context/rooms.context.js
@@ -0,0 +1,20 @@
+import { createContext, useEffect, useState } from 'react';
+import { database } from '../misc/firebase';
+import { transformToArrWithId } from '../misc/helpers';
+
+const RoomContext = createContext();
+
+export const RoomsProvider = ({ children }) => {
+  const [rooms, setRooms] = useState(null);
+  useEffect(() => {
+    const RoomListRef = database.ref('rooms');
+    RoomListRef.on('value', snap => {
+      const data = transformToArrWithId(snap.val());
+      setRooms(data);
+    });
+    return () => {
+      RoomListRef.off();
+    };
+  }, []);
+  return <RoomContext.Provider value={rooms}>{children}</RoomContext.Provider>;
+};
