8cde205cf8c890f9bd2f41b6ca9d3afea87db450
Aiyush Kumar <aiyushkanojia@gmail.com>
2023-09-29 16:08:45 +0530

    Create Button for ChatRooms

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,11 @@
 import DashboardToggle from './dashboard/DashboardToggle';
+import CreateRoomBtnModal from './CreateRoomBtnModal';
 const Sidebar = () => {
   return (
     <div className="h-100 pt-2">
       <div>
         <DashboardToggle />
+        <CreateRoomBtnModal />
       </div>
       bottom
     </div>
