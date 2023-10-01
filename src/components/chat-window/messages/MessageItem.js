ec006b23e99f6957b3f41ce05140e64ab2f61cdb
Aiyush Kumar <aiyushkanojia@gmail.com>
2023-10-01 12:01:39 +0530

    Show Profile Online Status Inside Text Msgs

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,13 +1,16 @@
 import ProfileAvatar from '../../ProfileAvatar';
 import TimeAgo from 'timeago-react';
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
+import PresenceDot from '../../PresenceDot';
 
 const MessageItem = ({ message }) => {
   const { author, createdAt, text } = message;
 
   return (
     <li className="padded mb-1">
-      <div className="d-flex align-item-center font-bolder mb-1">
+      <div className="d-flex align-items-center font-bolder mb-1">
+        <PresenceDot uid={author.uid} />
+
         <ProfileAvatar
           src={author.avatar}
           name={author.name}
